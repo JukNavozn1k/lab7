c46af10d11c8e947819bcaec53755b7203c957ed
JukNavozn1k <deffender1337@gmail.com>
2022-06-05 22:41:06 +0300

    added based struc (необходимо осуществить управление)

--- peano/frac.pas
+++ peano/frac.pas
@@ -1,6 +1,20 @@
 unit frac;
-// Модуль, в котором задаётся и отрисовывается фрактал
+
 interface 
+uses crt,wincrt,math,graph;
+type 
+    ptr = ^Dot;
+    Dot = record
+        CoordX,CoordY : integer;
+        Link : ptr;
+    end;
+procedure addNew();
 
 implementation
+procedure addNew();
+begin
+
+end;
+begin
+
 end.
